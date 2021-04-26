a1596e57be8699801c0a26f34a3ed77bbad62635
tahirozde <82972609+tahirozde@users.noreply.github.com>
2021-04-26 15:38:44 +0300

    Add files via upload

---
+++ hj/src/hj/helloJava.java
@@ -0,0 +1,37 @@
+package hj;
+
+public class helloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Hello word yazma
+		
+		
+ System.out.println("Hello word");
+ 
+ 
+           // DEÐÝÞKENLER 
+   int sayý = 11;
+   String mesaj ="öðrenci sayýsý";
+ System.out.println(mesaj+sayý);
+ System.out.println(mesaj+sayý);
+ System.out.println(mesaj+sayý);
+ System.out.println(mesaj+sayý);
+ System.out.println(mesaj+sayý);
+ 
+ 
+ 
+       //IF BLOKLARIYLA ÇALIÞMAK
+ 
+ int sayý2 = 20;
+ if (sayý2<20);
+    System.out.println("sayý 20 den küçük");
+
+		 
+ 
+     
+	}
+
+}
